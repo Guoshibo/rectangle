0a911bda9749e886dc0f88bf70f42a4e16c4d840
guoshibo <1137603852@qq.com>
2019-04-02 14:56:59 +0800

    feat

--- rectangle.js
+++ rectangle.js
@@ -1,23 +1,48 @@
-function Rectangle(e,a){
-  var t=Number(e),i=Number(a);
-  this.area=function(){
-    return t*i},
-    this.perimeter=function(){
-      return 2*(t+i)
+/* global Rectangle, validate, isLegalKey: true */
+$(function() {
+    var $width = $('#width'),
+        $height = $('#height'),
+        $btnCal = $('#calculate'),
+        $perimeter = $('#perimeter'),
+        $area = $('#area'),
+        $widthValidate = $('#width-validate'),
+        $heightValidate = $('#height-validate'),
+        isPassValidate = false;
+
+$width.keypress(function(e) {
+  if(/[abcdf-zABCDF-Z`~!@#$%^&*()=_+[\]{}|;:'",<>/?\\]/.test(e.key)) {
+          e.preventDefault();           
+  } 
+
+  if(e.key === '.'){
+    if(e.target.value === "") e.preventDefault();
+    if(e.target.value.indexOf('.')!==-1){
+      e.preventDefault();
+    }else{
+      if(e.target.selectionStart===0)e.preventDefault();
     }
-}
-function validate(e){
-  var a={isOK:!1,reason:""};
-  return""===e?a.reason="不能为空！":/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test(e)?Number(e)<0?a.reason="必须大于零":a.isOK=!0:a.reason="必须是数值",a
-}
-$(function(){
-  var a=$("#width"),t=$("#height"),e=$("#calculate"),i=$("#perimeter"),r=$("#area"),n=$("#width-validate"),l=$("#height-validate"),s=!1;
-  a.focusout(function(){
-    var e=validate(a.val());s=e.isOK,e.isOK?n.html(""):(n.html("宽度"+e.reason),a.select())
-  }),t.focusout(function(){
-    var e=validate(t.val());s=e.isOK,e.isOK?l.html(""):(l.html("高度"+e.reason),t.select())
-  }),e.click(function(){
-    if(s){var e=new Rectangle(a.val(),t.val());i.val(e.perimeter()),r.val(e.area())
-    }
-  })
+  } 
+});
+
+$height.keypress(function(e) {
+  if(!isLegalKey(e.key, e.target.value, e.target.selectionStart)) {
+          e.preventDefault();
+              
+  }
+    
+});
+
+$btnCal.click(function(){
+      if(!isPassValidate) return;
+
+          var w = $width.val(),
+          h = $height.val();
+
+    var r = new Rectangle(w, h);
+
+        $perimeter.val(r.perimeter());
+            $area.val(r.area());
+              
+});
+
 });
