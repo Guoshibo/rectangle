3af5b3d57fa48a562d3b11a4ddb35871e8d2c167
guoshibo <1137603852@qq.com>
2019-04-02 16:53:23 +0800

    field

--- rectangle.js
+++ rectangle.js
@@ -1,3 +1,8 @@
+function validate(e){
+    var a={isOK:!1,reason:""};
+    return""===e?a.reason="不能为空！":/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test(e)?Number(e)<0?a.reason="必须大于零":a.isOK=!0:a.reason="必须是数值",a
+}
+
 function Rectangle(e,a){
     var t=Number(e),i=Number(a);
     this.area=function(){
@@ -12,11 +17,35 @@ $(function() {
         $height = $('#height'),
         $calc = $('#calc'),
         $perimeter = $('#perimeter'),
-        $area = $('#area');
+        $area = $('#area'),
+        $widthValidate = $('#width-validate'),
+        $heightValidate = $('#height-validate'),
+        isPassValidate = false;
+
+    $width.focusout(function() {
+      var result = validate($width.val());
+          isPassValidate = result.isOK;
+          if(!result.isOK) {
+            $widthValidate.html('宽度' + result.reason);
+            $width.select();                    
+          } else {
+            $widthValidate.html('');      
+          }
+});
+
+    $height.focusout(function() {
+      var result = validate($height.val());
+          isPassValidate = result.isOK;
+          if(!result.isOK) {
+            $heightValidate.html('高度' + result.reason);
+            $height.select();     
+            } else {
+              $heightValidate.html('');           
+              }        
+    });
           
-  $calc.click(function(e){
-    e.preventDefault();
-    //这句代码是阻止网页提交之后的自动刷新
+  $calc.click(function(){
+    if(!isPassValidate) return;
     var w = $width.val(),
         h = $height.val();
     var r = new Rectangle(w, h);
@@ -26,8 +55,6 @@ $(function() {
 });
 
 });
-
-
 
 
 
