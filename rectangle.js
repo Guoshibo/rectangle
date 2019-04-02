81571f3c6b11cec3f37e5978da88a9b0d4cd1858
guoshibo <1137603852@qq.com>
2019-04-02 21:32:21 +0800

    char

--- rectangle.js
+++ rectangle.js
@@ -2,22 +2,94 @@ function Rectangle(e,a){
   var t=Number(e),i=Number(a);
   this.area=function(){
     return t*i},
-    this.perimeter=function(){
-      return 2*(t+i)
-    }
+  this.perimeter=function(){
+    return 2*(t+i)
+  }
 }
-function validate(e){
-  var a={isOK:!1,reason:""};
-  return""===e?a.reason="不能为空！":/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test(e)?Number(e)<0?a.reason="必须大于零":a.isOK=!0:a.reason="必须是数值",a
+
+
+function validate(data) {
+  var result = {
+    isOK: false,
+    reason: ''            
+  };
+  if(data === '') {
+        result.reason = '不能为空！';
+        return result;      
+  }
+  if(Number(data) < 0) {
+        result.reason = '必须大于零';
+        return result;           
+  }
+  result.isOK = true;
+  return result;
 }
+
 $(function(){
-  var a=$("#width"),t=$("#height"),e=$("#calculate"),i=$("#perimeter"),r=$("#area"),n=$("#width-validate"),l=$("#height-validate"),s=!1;
-  a.focusout(function(){
-    var e=validate(a.val());s=e.isOK,e.isOK?n.html(""):(n.html("宽度"+e.reason),a.select())
-  }),t.focusout(function(){
-    var e=validate(t.val());s=e.isOK,e.isOK?l.html(""):(l.html("高度"+e.reason),t.select())
-  }),e.click(function(){
-    if(s){var e=new Rectangle(a.val(),t.val());i.val(e.perimeter()),r.val(e.area())
-    }
-  })
+  var $width=$("#width"),
+      $height=$("#height"),
+      $btnCal=$("#calculate")
+      $perimeter=$("#perimeter"),
+      $widthValidate=$("#width-validate"),
+      $heightValidate=$("#height-validate"),
+      $area=$("#area"),
+      isPassValidate = false;
+
+$width.keypress(function(e) {
+  if(/[abcdf-zABCDF-Z`~!@#$%^&*()=_+[\]{}|;:'",<>/?\\]/.test(e.key)) {
+    e.preventDefault();            
+} 
+if(e.key === '.'){
+      if(e.target.value === "") e.preventDefault();
+      if(e.target.value.indexOf('.')!==-1){
+        e.preventDefault();     
+      }else{
+        if(e.target.selectionStart===0)e.preventDefault();          
+      }
+  }
+})
+
+$height.keypress(function(e) {
+  if(/[abcdf-zABCDF-Z`~!@#$%^&*()=_+[\]{}|;:'",<>/?\\]/.test(e.key)) {
+    e.preventDefault();           
+} 
+if(e.key === '.'){
+  if(e.target.value === "") e.preventDefault();
+  if(e.target.value.indexOf('.')!==-1){
+    e.preventDefault();                                      
+  }else{
+    if(e.target.selectionStart===0)e.preventDefault();                                      
+  }        
+}
+})
+
+
+$width.focusout(function() {
+          var result = validate($width.val());
+          isPassValidate = result.isOK;
+          if(!result.isOK) {
+          $widthValidate.html('宽度' + result.reason);
+          $width.select();                                                       
+          } else {
+              $widthValidate.html('');                              
+          }
+});
+$height.focusout(function() {
+          var result = validate($height.val());
+          isPassValidate = result.isOK;
+          if(!result.isOK) {
+            $heightValidate.html('高度' + result.reason);
+            $height.select();                                  
+          } else {
+            $heightValidate.html('');                                         
+          }               
+});
+
+$btnCal.click(function(){
+  var w = $width.val(),
+      h = $height.val();
+  var r = new Rectangle(w, h);
+  $perimeter.val(r.perimeter());
+  $area.val(r.area());   
+});
 });
