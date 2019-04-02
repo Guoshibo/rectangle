69b3ae0e31b75fa7632c7199ef34929f130d1f10
guoshibo <1137603852@qq.com>
2019-04-02 16:00:36 +0800

    h5

--- rectangle.js
+++ rectangle.js
@@ -1,3 +1,12 @@
+function Rectangle(e,a){
+    var t=Number(e),i=Number(a);
+    this.area=function(){
+      return t*i
+    },
+    this.perimeter=function(){
+      return 2*(t+i)
+    }
+}
 $(function() {
     var $width = $('#width'),
         $height = $('#height'),
@@ -5,7 +14,9 @@ $(function() {
         $perimeter = $('#perimeter'),
         $area = $('#area');
           
-  $calc.click(function(){
+  $calc.click(function(e){
+    e.preventDefault();
+    //这句代码是阻止网页提交之后的自动刷新
     var w = $width.val(),
         h = $height.val();
     var r = new Rectangle(w, h);
