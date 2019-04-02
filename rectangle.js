a5bda04cc763aa1caf88860188e2cf96cb553328
guoshibo <1137603852@qq.com>
2019-04-02 15:53:22 +0800

    h5

--- rectangle.js
+++ rectangle.js
@@ -1,48 +1,22 @@
-/* global Rectangle, validate, isLegalKey: true */
 $(function() {
     var $width = $('#width'),
         $height = $('#height'),
-        $btnCal = $('#calculate'),
+        $calc = $('#calc'),
         $perimeter = $('#perimeter'),
-        $area = $('#area'),
-        $widthValidate = $('#width-validate'),
-        $heightValidate = $('#height-validate'),
-        isPassValidate = false;
-
-$width.keypress(function(e) {
-  if(/[abcdf-zABCDF-Z`~!@#$%^&*()=_+[\]{}|;:'",<>/?\\]/.test(e.key)) {
-          e.preventDefault();           
-  } 
-
-  if(e.key === '.'){
-    if(e.target.value === "") e.preventDefault();
-    if(e.target.value.indexOf('.')!==-1){
-      e.preventDefault();
-    }else{
-      if(e.target.selectionStart===0)e.preventDefault();
-    }
-  } 
-});
-
-$height.keypress(function(e) {
-  if(!isLegalKey(e.key, e.target.value, e.target.selectionStart)) {
-          e.preventDefault();
-              
-  }
-    
-});
-
-$btnCal.click(function(){
-      if(!isPassValidate) return;
-
-          var w = $width.val(),
-          h = $height.val();
-
+        $area = $('#area');
+          
+  $calc.click(function(){
+    var w = $width.val(),
+        h = $height.val();
     var r = new Rectangle(w, h);
-
-        $perimeter.val(r.perimeter());
-            $area.val(r.area());
+    $perimeter.val(r.perimeter());
+    $area.val(r.area());
               
 });
 
 });
+
+
+
+
+
